16be22493895071c8edbfec9dd765b20fe497bc5
pfaffic <pfaffic@users.noreply.github.com>
2020-05-25 01:26:47 +0200

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import '../App.css';
+import React from "react";
+import "../App.css";
 
 export default function Footer() {
   return (
@@ -11,13 +11,13 @@ export default function Footer() {
         Christoph Pfaffmann
       </div>
       <div>
-        Daten bereitgestellt von{' '}
+        Daten bereitgestellt von{" "}
         <a
           href="https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/RKI_COVID19/FeatureServer/0/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          RKI_COVID19
+          RKI COVID19
         </a>
       </div>
       <div>2020</div>
