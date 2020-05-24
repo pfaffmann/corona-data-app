54e97ebf6714d47923fb1881b11c59dafb649564
pfaffic <48207288+pfaffic@users.noreply.github.com>
2020-05-24 20:35:44 +0200

    fixed all Data page switch

--- src/App.js
+++ src/App.js
@@ -3,35 +3,77 @@ import FullDataTable from './components/FullDataTable';
 import './App.css';
 import Button from '@material-ui/core/Button';
 
-const API_URL =
-  'https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/RKI_COVID19/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json';
+const OBJECT_ID_API_URL =
+  'https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/RKI_COVID19/FeatureServer/0/query?where=1%3D1&objectIds=&time=&resultType=none&outFields=*&returnIdsOnly=true&returnUniqueIdsOnly=false&returnCountOnly=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&sqlFormat=none&f=pjson&token=';
 
 const coronaDataSchema = {
-  objectIdFieldName: '',
-  uniqueIdField: {},
-  globalIdFieldName: '',
-  fields: [],
+  objectIdFieldName: 'defew',
+  uniqueIdField: { a: 'fefwef', b: 'frrg' },
+  globalIdFieldName: 'fwefwef',
+  fields: [1, 2, 3, 4, 5, 6],
   exceededTransferLimit: false,
-  features: [],
+  features: [1, 2, 3, 4, 5, 6, 7, 8],
 };
+
+const test = [coronaDataSchema, coronaDataSchema, coronaDataSchema];
 
 function App() {
   const [loading, setLoading] = useState(false);
-  const [coronaData, setCoronaData] = useState(coronaDataSchema);
+  const [coronaData, setCoronaData] = useState([]);
   const [showDataTable, setShowDataTable] = useState(false);
+  const [objectIds, setObjectIds] = useState([]);
 
   useEffect(() => {
     setLoading(true);
-
     const fetchData = async () => {
-      const result = await fetch(API_URL);
-      const data = await result.json();
-      setCoronaData(data);
-      console.log(data);
+      let result = await fetch(OBJECT_ID_API_URL);
+      let data = await result.json();
+      setObjectIds(data.objectIds); //optional
+      const maxSchranke = data.objectIds[data.objectIds.length - 1] - 1;
+      const fetchCycles = Math.ceil(data.objectIds.length / 5000);
+      //console.log(fetchCycles);
+      for (let cycle = 0; cycle < fetchCycles; cycle++) {
+        let untereSchranke = data.objectIds[cycle * 5000];
+        let obereSchranke =
+          untereSchranke + 5000 < maxSchranke
+            ? untereSchranke + 5000
+            : maxSchranke;
+        //console.log(`untere Schranke: ${untereSchranke}`);
+        //console.log(`obere  Schranke: ${obereSchranke}`);
+
+        let res = await fetch(
+          `https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/RKI_COVID19/FeatureServer/0/query?where=objectId+>%3D+${untereSchranke}+AND+objectId+<+${obereSchranke}&objectIds=&time=&resultType=none&outFields=*&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&sqlFormat=none&f=pjson&token=`
+        );
+        let d = await res.json();
+        //console.log(d);
+        coronaData.push(d);
+      }
       setLoading(false);
     };
     fetchData();
   }, []);
+
+  // useEffect(() => {
+  //   setLoading(true);
+  //   const fetchData = async () => {
+  //     const result = await fetch(
+  //       `https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/RKI_COVID19/FeatureServer/0/query?where=objectId+>%3D+${8389849}+AND+objectId+<+${8389949}&objectIds=&time=&resultType=none&outFields=*&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&sqlFormat=none&f=pjson`
+  //     );
+  //     console.log(result);
+  //     const data = await result.json();
+  //     setCoronaData(data);
+  //     console.log(data);
+  //     setLoading(false);
+  //   };
+  //   fetchData();
+  // }, []);
+  // console.log(test);
+  // let i = 1;
+  // test.map((t) =>
+  //   t.fields.map((x) => {
+  //     console.log(i++);
+  //   })
+  // );
 
   return (
     <div className="App">
@@ -49,13 +91,14 @@ function App() {
           variant="outlined"
           color="primary"
           onClick={() => {
+            console.log(coronaData);
             setShowDataTable(true);
           }}
         >
           Alle Daten anzeigen
         </Button>
       </div>
-      {coronaData.fields.length > 0 && showDataTable ? (
+      {coronaData.length > 0 && showDataTable ? (
         <div className="App-table">
           <FullDataTable data={coronaData}></FullDataTable>
         </div>
--- src/components/FullDataTable.js
+++ src/components/FullDataTable.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
@@ -7,8 +7,14 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Slider from '@material-ui/core/Slider';
 
 const useStyles = makeStyles({
+  slider: {
+    width: 300,
+  },
   table: {
     minWidth: 650,
   },
@@ -18,49 +24,91 @@ const toDate = (intDate) => {
   return new Date(intDate).toLocaleString('de-DE', { dateStyle: 'short' });
 };
 
+function valuetext(value) {
+  return `S. ${value}`;
+}
+
 export default function FullDataTable(props) {
   const classes = useStyles();
-  const data = props.data;
+  const coronaData = props.data;
+  const [notShowData, setNotShowData] = useState(true);
+  const [page, setPage] = useState(1);
+  const anzData =
+    (coronaData.length - 1) * 5000 +
+    coronaData[coronaData.length - 1].features.length;
 
   return (
-    <>
-      {/* <div>{data.features[1].attributes.Meldedatum}</div>
-      <div>{toDate(data.features[1].attributes.Meldedatum)}</div> */}
-      <TableContainer component={Paper}>
-        <Table className={classes.table} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              {data.fields.map((field) => (
-                <TableCell>{field.name}</TableCell>
-              ))}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {data.features.map((feature) => (
-              <TableRow key={feature.attributes.ObjectId}>
-                <TableCell>{feature.attributes.IdBundesland}</TableCell>
-                <TableCell>{feature.attributes.Bundesland}</TableCell>
-                <TableCell>{feature.attributes.Landkreis}</TableCell>
-                <TableCell>{feature.attributes.Altersgruppe}</TableCell>
-                <TableCell>{feature.attributes.Geschlecht}</TableCell>
-                <TableCell>{feature.attributes.AnzahlFall}</TableCell>
-                <TableCell>{feature.attributes.AnzahlTodesfall}</TableCell>
-                <TableCell>{feature.attributes.ObjectId}</TableCell>
-                <TableCell>{toDate(feature.attributes.Meldedatum)}</TableCell>
-                <TableCell>{feature.attributes.IdLandkreis}</TableCell>
-                <TableCell>{feature.attributes.Datenstand}</TableCell>
-                <TableCell>{feature.attributes.NeuerFall}</TableCell>
-                <TableCell>{feature.attributes.NeuerTodesfall}</TableCell>
-                <TableCell>{toDate(feature.attributes.Refdatum)}</TableCell>
-                <TableCell>{feature.attributes.NeuGenesen}</TableCell>
-                <TableCell>{feature.attributes.AnzahlGenesen}</TableCell>
-                <TableCell>{feature.attributes.IstErkrankungsbeginn}</TableCell>
-                <TableCell>{feature.attributes.Altersgruppe2}</TableCell>
+    <div>
+      <p>
+        Zeilen: {(page - 1) * 5000} bis{' '}
+        {page * 5000 < anzData ? page * 5000 : anzData} von {anzData}
+      </p>
+      <div className={classes.slider}>
+        <Typography id="discrete-slider" gutterBottom>
+          Seite: {page}
+        </Typography>
+        <Slider
+          defaultValue={page}
+          getAriaValueText={valuetext}
+          aria-labelledby="discrete-slider"
+          valueLabelDisplay="auto"
+          step={1}
+          marks
+          min={1}
+          max={coronaData.length}
+          onChange={(e, value) => {
+            setPage(value);
+            setNotShowData(true);
+          }}
+        />
+      </div>
+      {notShowData ? (
+        <Button
+          variant="outlined"
+          color="secondary"
+          onClick={() => setNotShowData(false)}
+        >
+          Zeilen anzeigen
+        </Button>
+      ) : (
+        <TableContainer component={Paper}>
+          <Table className={classes.table} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                {coronaData[0].fields.map((field) => (
+                  <TableCell>{field.name}</TableCell>
+                ))}
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </>
+            </TableHead>
+            <TableBody>
+              {coronaData[page - 1].features.map((feature) => (
+                <TableRow key={feature.attributes.ObjectId}>
+                  <TableCell>{feature.attributes.IdBundesland}</TableCell>
+                  <TableCell>{feature.attributes.Bundesland}</TableCell>
+                  <TableCell>{feature.attributes.Landkreis}</TableCell>
+                  <TableCell>{feature.attributes.Altersgruppe}</TableCell>
+                  <TableCell>{feature.attributes.Geschlecht}</TableCell>
+                  <TableCell>{feature.attributes.AnzahlFall}</TableCell>
+                  <TableCell>{feature.attributes.AnzahlTodesfall}</TableCell>
+                  <TableCell>{feature.attributes.ObjectId}</TableCell>
+                  <TableCell>{toDate(feature.attributes.Meldedatum)}</TableCell>
+                  <TableCell>{feature.attributes.IdLandkreis}</TableCell>
+                  <TableCell>{feature.attributes.Datenstand}</TableCell>
+                  <TableCell>{feature.attributes.NeuerFall}</TableCell>
+                  <TableCell>{feature.attributes.NeuerTodesfall}</TableCell>
+                  <TableCell>{toDate(feature.attributes.Refdatum)}</TableCell>
+                  <TableCell>{feature.attributes.NeuGenesen}</TableCell>
+                  <TableCell>{feature.attributes.AnzahlGenesen}</TableCell>
+                  <TableCell>
+                    {feature.attributes.IstErkrankungsbeginn}
+                  </TableCell>
+                  <TableCell>{feature.attributes.Altersgruppe2}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      )}
+    </div>
   );
 }
